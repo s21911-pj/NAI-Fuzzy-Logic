9535171f38567633bf780f1338b1eef86b4da806
Piotr <s21911@pjwstk.edu.pl>
2022-11-02 18:58:03 +0100

    changed to english language

--- main.py
+++ main.py
@@ -8,66 +8,66 @@ from skfuzzy import control as ctrl
 
 
 
-praceDomowe = ctrl.Antecedent(np.arange(0, 11, 1), 'praceDomowe')
-kolokwia = ctrl.Antecedent(np.arange(0, 11, 1), 'kolokwia')
-aktywnosc = ctrl.Antecedent(np.arange(0, 11, 1), 'aktywnosc')
-ocena = ctrl.Consequent(np.arange(0, 50, 1), 'ocena')
+home_work = ctrl.Antecedent(np.arange(0, 11, 1), 'home_work')
+colloquia = ctrl.Antecedent(np.arange(0, 11, 1), 'colloquia')
+activity = ctrl.Antecedent(np.arange(0, 11, 1), 'activity')
+rating = ctrl.Consequent(np.arange(0, 50, 1), 'rating')
 
 
-praceDomowe.automf(5)
-kolokwia.automf(5)
-aktywnosc.automf(5)
+home_work.automf(7)
+colloquia.automf(7)
+activity.automf(7)
 
 
-ocena['1'] = fuzz.trimf(ocena.universe, [0, 0, 13])
-ocena['2'] = fuzz.trimf(ocena.universe, [0, 13, 25])
-ocena['3'] = fuzz.trimf(ocena.universe, [13, 25, 25])
-ocena['4'] = fuzz.trimf(ocena.universe, [25, 37, 37])
-ocena['5'] = fuzz.trimf(ocena.universe, [37, 45, 45])
-ocena['6'] = fuzz.trimf(ocena.universe, [45, 49, 49])
-
-
-
-praceDomowe['average'].view()
-
-kolokwia.view()
-
-aktywnosc.view()
-
-ocena.view()
-
-
-rule1 = ctrl.Rule(praceDomowe['poor'] | aktywnosc['poor'] | kolokwia['poor'], ocena['1'])
-rule2 = ctrl.Rule(praceDomowe['average'] | aktywnosc['average'] | kolokwia['average'], ocena['2'])
-rule3 = ctrl.Rule(praceDomowe['poor'] | aktywnosc['average'] | kolokwia['average'], ocena['3'])
-rule4 = ctrl.Rule(praceDomowe['good'] | aktywnosc['good'] | kolokwia['good'], ocena['4'])
-rule5 = ctrl.Rule(praceDomowe['average'] | aktywnosc['good'] | kolokwia['good'], ocena['5'])
-rule6 = ctrl.Rule(praceDomowe['good'] | aktywnosc['good'] | kolokwia['good'], ocena['6'])
+rating['1'] = fuzz.trimf(rating.universe, [0, 0, 13])
+rating['2'] = fuzz.trimf(rating.universe, [0, 13, 25])
+rating['3'] = fuzz.trimf(rating.universe, [13, 25, 25])
+rating['4'] = fuzz.trimf(rating.universe, [25, 37, 37])
+rating['5'] = fuzz.trimf(rating.universe, [37, 45, 45])
+rating['6'] = fuzz.trimf(rating.universe, [45, 49, 49])
 
 
 
+home_work['average'].view()
+
+colloquia.view()
+
+activity.view()
+
+rating.view()
+
+
+rule1 = ctrl.Rule(home_work['poor'] | activity['poor'] | colloquia['poor'], rating['1'])
+rule2 = ctrl.Rule(home_work['average'] | activity['average'] | colloquia['average'], rating['2'])
+rule3 = ctrl.Rule(home_work['poor'] | activity['average'] | colloquia['average'], rating['3'])
+rule4 = ctrl.Rule(home_work['good'] | activity['good'] | colloquia['good'], rating['4'])
+rule5 = ctrl.Rule(home_work['average'] | activity['good'] | colloquia['good'], rating['5'])
+rule6 = ctrl.Rule(home_work['good'] | activity['good'] | colloquia['excellent'], rating['6'])
 
 
 
 
-ocenianie_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5, rule6])
 
 
 
-ocenianie = ctrl.ControlSystemSimulation(ocenianie_ctrl)
+assessment_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5, rule6])
 
 
 
-ocenianie.input['kolokwia'] = 1
-ocenianie.input['aktywnosc'] = 1
-ocenianie.input['praceDomowe'] = 1
+assessment = ctrl.ControlSystemSimulation(assessment_ctrl)
 
 
-ocenianie.compute()
+
+assessment.input['colloquia'] = 9
+assessment.input['activity'] = 9
+assessment.input['home_work'] = 9
 
 
-print (ocenianie.output['ocena'])
-ocena.view(sim=ocenianie)
+assessment.compute()
+
+
+print (assessment.output['rating'])
+rating.view(sim=assessment)
 
 
 plt.show() 
