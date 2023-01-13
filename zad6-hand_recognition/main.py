2da2ee3bde8e6453b0ee59059c11465130c6760e
sir-hector <krauskarol7@gmail.com>
2023-01-13 13:19:28 +0100

    init zad6

---
+++ zad6-hand_recognition/main.py
@@ -0,0 +1,16 @@
+# Authors:
+# Karol Kraus s20687
+# Piotr Mastalerz s21911
+
+
+# environmental instructions
+# create venv
+#   python3 -m venv venv
+# activate venv
+#   source venv/bin/activate
+# install packages
+#   pip3 install -r requirements.txt
+# run app
+#   python3 main.py
+
+print('Zad 6')
